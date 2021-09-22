e4dd93edb8bb61d98e72a8fabe5194ca1161e02f
Andi Pätzold <github@andipaetzold.com>
2021-09-23 01:51:18 +0200

    refactor: extract LineChart

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,16 +1,7 @@
 import { Typography } from "@mui/material";
-import uniq from "lodash/uniq";
 import React, { Fragment } from "react";
-import {
-  CartesianGrid,
-  Line,
-  LineChart,
-  ResponsiveContainer,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from "recharts";
-import { COLORS, STREAM_TYPES } from "./constants";
+import { LineChart } from "./charts/LineChart";
+import { STREAM_TYPES } from "./constants";
 import { Stream } from "./types";
 
 interface Props {
@@ -27,75 +18,11 @@ export function Editor({ streams }: Props) {
           return null;
         }
 
-        const timestamps = uniq(
-          filteredStreams.flatMap((s) => s.data.map((d) => d.timestamp))
-        );
-
-        const firstTimestamp = Math.min(...timestamps);
-
-        const data = timestamps.map((t) => ({
-          timestamp: (t - firstTimestamp) / 60,
-
-          ...Object.fromEntries(
-            filteredStreams
-              .map((s, si) => {
-                // @ts-ignore
-                const value = s.data.find((d) => d.timestamp === t)?.value;
-                if (value !== undefined) {
-                  return [`s${si}`, value];
-                }
-
-                return null;
-              })
-              .filter((s): s is any[] => s !== null)
-          ),
-        }));
-
         return (
           <Fragment key={type}>
             <Typography variant="h3">{type}</Typography>
 
-            <div style={{ width: "100%", height: 300 }}>
-              <ResponsiveContainer>
-                <LineChart
-                  data={data}
-                  margin={{
-                    top: 5,
-                    right: 30,
-                    left: 20,
-                    bottom: 5,
-                  }}
-                >
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis
-                    dataKey="timestamp"
-                    type="number"
-                    domain={[
-                      "dataMin",
-                      (dataMax: number) => Math.ceil(dataMax),
-                    ]}
-                    allowDecimals={false}
-                    tickFormatter={(timestamp) => `${timestamp}min`}
-                  />
-                  <YAxis
-                    domain={["dataMin", "dataMax"]}
-                    type="number"
-                    allowDecimals={false}
-                  />
-                  <Tooltip />
-                  {filteredStreams.map((_s, si) => (
-                    <Line
-                      key={si}
-                      type="monotone"
-                      dataKey={`s${si}`}
-                      isAnimationActive={false}
-                      dot={false}
-                      stroke={COLORS[si % COLORS.length]}
-                    />
-                  ))}
-                </LineChart>
-              </ResponsiveContainer>
-            </div>
+            <LineChart streams={filteredStreams} />
           </Fragment>
         );
       })}
---
+++ src/charts/LineChart.tsx
@@ -0,0 +1,83 @@
+import {
+  CartesianGrid,
+  Line,
+  LineChart as LineChartComponent,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import { COLORS } from "../constants";
+import { Stream } from "../types";
+import uniq from "lodash/uniq";
+
+interface Props {
+  streams: Stream[];
+}
+
+export function LineChart({ streams }: Props) {
+  const timestamps = uniq(
+    streams.flatMap((stream) => stream.data.map((d) => d.timestamp))
+  );
+
+  const firstTimestamp = Math.min(...timestamps);
+
+  const data = timestamps.map((t) => ({
+    timestamp: (t - firstTimestamp) / 60,
+
+    ...Object.fromEntries(
+      streams
+        .map((stream, si) => {
+          // @ts-ignore
+          const value = stream.data.find((d) => d.timestamp === t)?.value;
+          if (value !== undefined) {
+            return [`s${si}`, value];
+          }
+
+          return null;
+        })
+        .filter((s): s is any[] => s !== null)
+    ),
+  }));
+
+  return (
+    <div style={{ width: "100%", height: 300 }}>
+      <ResponsiveContainer>
+        <LineChartComponent
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="timestamp"
+            type="number"
+            domain={["dataMin", (dataMax: number) => Math.ceil(dataMax)]}
+            allowDecimals={false}
+            tickFormatter={(timestamp) => `${timestamp}min`}
+          />
+          <YAxis
+            domain={["dataMin", "dataMax"]}
+            type="number"
+            allowDecimals={false}
+          />
+          <Tooltip />
+          {streams.map((_s, si) => (
+            <Line
+              key={si}
+              type="monotone"
+              dataKey={`s${si}`}
+              isAnimationActive={false}
+              dot={false}
+              stroke={COLORS[si % COLORS.length]}
+            />
+          ))}
+        </LineChartComponent>
+      </ResponsiveContainer>
+    </div>
+  );
+}
