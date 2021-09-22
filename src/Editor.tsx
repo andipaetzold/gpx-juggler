94b58d367375b9b5192688fc599b9dfd094416bb
Andi Pätzold <github@andipaetzold.com>
2021-09-23 01:52:57 +0200

    feat: add proper title for charts

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material";
 import React, { Fragment } from "react";
 import { LineChart } from "./charts/LineChart";
 import { STREAM_TYPES } from "./constants";
-import { Stream } from "./types";
+import { Stream, StreamType } from "./types";
 
 interface Props {
   streams: Stream[];
@@ -20,7 +20,7 @@ export function Editor({ streams }: Props) {
 
         return (
           <Fragment key={type}>
-            <Typography variant="h3">{type}</Typography>
+            <Typography variant="h3">{streamNames[type]}</Typography>
 
             <LineChart streams={filteredStreams} />
           </Fragment>
@@ -29,3 +29,11 @@ export function Editor({ streams }: Props) {
     </>
   );
 }
+
+const streamNames: { [type in StreamType]: string } = {
+  cadence: "Cadence",
+  elevation: "elevation",
+  heartrate: "Heart Rate",
+  power: "Power",
+  coordinate: "Map",
+};
